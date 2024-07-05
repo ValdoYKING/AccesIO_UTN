df1a8ed5dc7bce98404f67c3f187fb4dcd41354c
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-07-05 05:14:00 -0600

    Nuevas vistas

---
+++ app/src/main/java/com/pixelfusion/accesio_utn/view/PerfilView.kt
@@ -0,0 +1,62 @@
+package com.pixelfusion.accesio_utn.view
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.pixelfusion.accesio_utn.components.ButtonNext
+import com.pixelfusion.accesio_utn.components.DrawerContent
+import com.pixelfusion.accesio_utn.components.SuperiorData
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun PerfilView(navController: NavController) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            DrawerContent(navController)
+        },
+        drawerState = drawerState,
+        content = {
+            Scaffold(
+                topBar = {
+                    SuperiorData(drawerState, scope)
+                },
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Mi perfil (view)",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    ButtonNext(navController, "home_user_view")
+                }
+            }
+        }
+    )
+}
