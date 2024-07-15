1fd82bb51abdbee1d0d37770010dcc675c09419b
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-07-15 14:39:45 -0600

    Optimizacion de dependencias

--- app/src/main/java/com/pixelfusion/accesio_utn/MainActivity.kt
+++ app/src/main/java/com/pixelfusion/accesio_utn/MainActivity.kt
@@ -3,48 +3,10 @@ package com.pixelfusion.accesio_utn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.google.firebase.FirebaseApp
 import com.pixelfusion.accesio_utn.components.DynamicStatusBarColor
 import com.pixelfusion.accesio_utn.navigation.AppNavigation
 import com.pixelfusion.accesio_utn.ui.theme.AccesIOUTNTheme
-import com.pixelfusion.accesio_utn.util.Constants
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
 
@@ -54,10 +16,8 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent { 
             AccesIOUTNTheme {
-                //MyApp()
                 DynamicStatusBarColor()
                 AppNavigation()
-                //FlipCard()
             }
         }
     }
