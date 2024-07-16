41706b09480000b15d701d4dabb9e937ed7ac6ad
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-07-16 01:19:06 -0600

    Nueva vista para actualizacion de fotografia

---
+++ app/src/main/java/com/pixelfusion/accesio_utn/viewmodel/ImageUserUpdateViewModel.kt
@@ -0,0 +1,78 @@
+package com.pixelfusion.accesio_utn.viewmodel
+
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.launch
+
+class ImageUserUpdateViewModel : ViewModel() {
+    private lateinit var auth: FirebaseAuth
+    private val storage = Firebase.storage
+    private val database = Firebase.database.reference
+
+    var isLoading by mutableStateOf(false)
+        private set
+
+    fun fetchData() {
+        viewModelScope.launch {
+            try {
+                isLoading = true
+                //uploadImageAndUpdateUser()
+            } catch (e: Exception) {
+                println("Error ${e.message}")
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+
+    init {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun uploadImageAndUpdateUser(
+        context: Context,
+        imageUri: Uri,
+        onSuccess: () -> Unit
+    ) {
+        isLoading = true // Establecer en true al iniciar la carga
+        val userId = auth.currentUser?.uid ?: return
+        val storageRef =
+            storage.reference.child("user_images/${userId}/${imageUri.lastPathSegment}")
+
+        val uploadTask = storageRef.putFile(imageUri)
+
+        uploadTask.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                database.child("users").child(userId).child("image_path").setValue(uri.toString())
+                    .addOnSuccessListener {
+                        showToast(context, "Imagen subida y registro actualizado")
+                        onSuccess()
+                    }
+                    .addOnFailureListener { exception ->
+                        showToast(context, "Error al actualizar el registro: ${exception.message}")
+                    }
+            }.addOnFailureListener { exception ->
+                showToast(context, "Error al obtener la URL de descarga: ${exception.message}")
+            }
+        }.addOnFailureListener { exception ->
+            showToast(context, "Error al subir la imagen: ${exception.message}")
+        }.addOnCompleteListener {
+            isLoading = false // Establecer en false al finalizar la carga
+        }
+    }
+
+}
