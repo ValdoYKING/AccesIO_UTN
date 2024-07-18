96a67c2072b457496f4c2309560b8d8dcf2366bc
Laura Acosta <231271037@alumnos.utn.edu.mx>
2024-07-18 13:59:11 -0600

    ya esta el perfil solo falta la opcion de actualizar foto y editar informacion de usuario

--- app/src/main/java/com/pixelfusion/accesio_utn/view/PerfilView.kt
+++ app/src/main/java/com/pixelfusion/accesio_utn/view/PerfilView.kt
@@ -1,8 +1,6 @@
 package com.pixelfusion.accesio_utn.view
 
-import UserProfileViewModel
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,193 +11,177 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import coil.compose.rememberImagePainter
 import com.pixelfusion.accesio_utn.R
-import com.pixelfusion.accesio_utn.components.ButtonNext
 import com.pixelfusion.accesio_utn.components.ContenidoSuperior
 import com.pixelfusion.accesio_utn.components.DrawerContent3
-import com.pixelfusion.accesio_utn.components.SuperiorData
+import com.pixelfusion.accesio_utn.ui.theme.utnGreen
+import com.pixelfusion.accesio_utn.viewmodel.UserProfileViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun PerfilView ( navController: NavController,
-viewModel: UserProfileViewModel) {
+fun PerfilView(
+    navController: NavController,
+    viewModelU: UserProfileViewModel
+) {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = currentBackStackEntry?.destination?.route
+    var showDialog by remember { mutableStateOf(false) }
 
+    LaunchedEffect(Unit) {
+        viewModelU.fetchData()
+    }
 
-        val user = viewModel.userState
-
-            Scaffold(
-                topBar = { TopBarProfile() }
-            ) { innerPadding ->
-                LazyColumn(
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent3(navController, currentRoute)
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                ContenidoSuperior(drawerState, scope, navController)
+            },
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+                    .verticalScroll(rememberScrollState()), // Habilitar scroll vertical
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
                     modifier = Modifier
-                        .padding(innerPadding)
-                        .padding(16.dp)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                        .fillMaxWidth()
+                        .padding(top = 50.dp), // Aquí puedes cambiar el margen superior
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    item { Spacer(modifier = Modifier.height(30.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.nombre,
-                            onValueChange = { viewModel.onValueChange(it, "nombre") },
-                            label = { Text("Nombre") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-                            keyboardActions = KeyboardActions(onNext = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.apellido,
-                            onValueChange = { viewModel.onValueChange(it, "apellido") },
-                            label = { Text("Apellidos") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-                            keyboardActions = KeyboardActions(onNext = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.matricula,
-                            onValueChange = { viewModel.onValueChange(it, "matricula") },
-                            label = { Text("Matrícula") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Number,
-                                imeAction = ImeAction.Next
-                            ),
-                            keyboardActions = KeyboardActions(onNext = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.carrera,
-                            onValueChange = { viewModel.onValueChange(it, "carrera") },
-                            label = { Text("Carrera") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-                            keyboardActions = KeyboardActions(onNext = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.correo_electronico,
-                            onValueChange = { viewModel.onValueChange(it, "correo_electronico") },
-                            label = { Text("Correo electrónico") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Email,
-                                imeAction = ImeAction.Next
-                            ),
-                            keyboardActions = KeyboardActions(onNext = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-                    item {
-                        OutlinedTextField(
-                            value = user.num_seguro_social,
-                            onValueChange = { viewModel.onValueChange(it, "num_seguro_social") },
-                            label = { Text(text = "Número seguro social") },
-                            modifier = Modifier.fillMaxWidth(),
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Number,
-                                imeAction = ImeAction.Done
-                            ),
-                            keyboardActions = KeyboardActions(onDone = { /* No action */ })
-                        )
-                    }
-                    item { Spacer(modifier = Modifier.height(16.dp)) }
-                    item {
-                        Button(onClick = {
-                            // Lógica para cerrar sesión
-                            viewModel.logout()
-                            navController.navigate("login_screen") {
-                                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    val imagePath = viewModelU.stateHome.image_path ?: ""
+                    if (imagePath.isNotEmpty()) {
+                        val imagePainter: Painter = rememberImagePainter(
+                            data = imagePath,
+                            builder = {
+                                crossfade(true)
+                                placeholder(R.drawable.app_fondo)
                             }
-                        }) {
-                            Text(text = "Cerrar Sesión")
-                        }
+                        )
+
+                        Image(
+                            painter = imagePainter,
+                            contentDescription = "Foto de perfil",
+                            modifier = Modifier
+                                .size(150.dp)
+                                .padding(8.dp), // Aquí puedes cambiar el margen alrededor de la imagen
+                            contentScale = ContentScale.Crop,
+                            alignment = Alignment.Center,
+                        )
                     }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Mi Perfil",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = utnGreen,
+                        modifier = Modifier.padding(bottom = 16.dp), // Aquí puedes cambiar el margen inferior
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    ProfileDetailsView(viewModelU)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        // Acción para cerrar sesión
+                        viewModelU.logout(navController)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(text = "Cerrar Sesión")
                 }
             }
-}
-@Composable
-fun TopBarProfile() {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .height(150.dp),
-        shape = RoundedCornerShape(16.dp),
-        color = Color(0xFF0D9462),
-        border = BorderStroke(2.dp, Color(0xFF0D9462)),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Image(
-                painter = painterResource(id = R.drawable.utnfoto),
-                contentDescription = null,
-                modifier = Modifier.size(50.dp),
-                contentScale = ContentScale.Fit
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Mi Perfil",
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
         }
     }
 }
 
+@Composable
+fun ProfileDetailsView(profileData: UserProfileViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        ProfileDetailItem(label = "Nombre", value = "${profileData.stateHome.nombre} ${profileData.stateHome.apellido}")
+        ProfileDetailItem(label = "Matrícula", value = profileData.stateHome.matricula)
+        ProfileDetailItem(label = "Correo Electrónico", value = profileData.stateHome.correo_electronico)
+        ProfileDetailItem(label = "Teléfono", value = profileData.stateHome.telefono)
+        ProfileDetailItem(label = "Carrera", value = profileData.stateHome.carrera)
+        ProfileDetailItem(label = "Rol", value = profileData.stateHome.id_rol)
+    }
+}
 
-
-
-
+@Composable
+fun ProfileDetailItem(label: String, value: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp) // Puedes ajustar el padding para separar los elementos
+    ) {
+        Text(
+            text = "$label:",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = Color.Black
+        )
+        Text(
+            text = value,
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
